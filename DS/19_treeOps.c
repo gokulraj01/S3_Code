7480a880de2a9652b5a039e31499a91ed051ea87
rogue-geek <84265206+rogue-geek@users.noreply.github.com>
2022-03-08 11:23:45 +0530

    TreeOps added

--- DS/19_treeOps.c
+++ DS/19_treeOps.c
@@ -63,9 +63,7 @@ void deleteNode(struct Node *tree, char delNode){
     struct Node *res = searchParent(NULL, tree, delNode);
     struct Node *temp;
     if(res != NULL){
-        printf("Got parent as %c", res->data);
         if(res->lhs->data == delNode && res->lhs->lhs == NULL && res->lhs->rhs == NULL){
-            printf("Child is LEFT");
             temp = res->lhs;
             res->lhs = NULL;
             free(temp);
@@ -73,7 +71,6 @@ void deleteNode(struct Node *tree, char delNode){
         else
             printf("Node not leaf!!\n");
         if(res->rhs->data == delNode && res->rhs->lhs == NULL && res->rhs->rhs == NULL){
-            printf("Child is RIGHT");
             temp = res->rhs;
             res->rhs = NULL;
             free(temp);
@@ -86,7 +83,7 @@ void deleteNode(struct Node *tree, char delNode){
 }
 
 void insertNode(struct Node *tree, char root, char nodeName){
-    struct Node *res = searchNode(tree, root);
+    struct Node *res = searchTree(tree, root);
     if(res->lhs == NULL || res->rhs == NULL){
         struct Node *newNode = malloc(sizeof(struct Node));
         newNode->lhs = NULL;
@@ -103,10 +100,83 @@ void insertNode(struct Node *tree, char root, char nodeName){
         printf("Node Full!!\n");
 }
 
+void showTree(char *msg, struct Node *tree){
+    printf("\n%s\n", msg);
+    struct Node *nodes[2][10];
+    int ni = 1, buffi = 0, validCount = 1;
+    nodes[buffi][0] = tree;
+    char opBuff[20];
+    opBuff[0] = '\0';
+    while(validCount > 0){
+        printf("%s", opBuff);
+        int newBuff = !buffi;
+        int len = ni, opi = 0;
+        validCount = 0; ni = 0;
+        for(int i=0; i<len; i++){
+            if(nodes[buffi][i] == NULL){
+                opBuff[opi++] = 254;
+                opBuff[opi++] = ' ';
+                nodes[newBuff][ni++] = NULL;
+                nodes[newBuff][ni++] = NULL;
+            }
+            else{
+                validCount++;
+                opBuff[opi++] = nodes[buffi][i]->data;
+                opBuff[opi++] = ' ';
+                nodes[newBuff][ni++] = nodes[buffi][i]->lhs;
+                nodes[newBuff][ni++] = nodes[buffi][i]->rhs;
+            }
+        }
+        opBuff[opi++] = '\n';
+        opBuff[opi] = '\0';
+        buffi = newBuff;
+    }
+}
+
 void main(){
+    printf("Binary Tree Operations\nCreate a tree!!\n");
     printf("Enter Root Node Name: ");
     struct Node *tree = makeTree(inputChar());
-    printf("Enter Node Name to find: ");
-    char k = inputChar();
-    struct 
+    showTree("Original Tree", tree);
+    printf("\nMenu\n[1] Add\t\t[2] Delete\n[3] Search Node\t[4] Search Parent\n[5] Show Tree\t[6] EXIT\n");
+    int looping = 1;
+    struct Node *res;
+    while(looping){
+        int choice;
+        printf("\nChoice: ");
+        scanf("%d", &choice);
+        char src, dest;
+        switch(choice){
+            case 1:
+                printf("New Node Name: ");
+                src = inputChar();
+                printf("Destination Node: ");
+                dest = inputChar(); 
+                insertNode(tree, dest, src);
+                break;
+            case 2:
+                printf("Node to remove: ");
+                src = inputChar();
+                deleteNode(tree, src);
+                break;
+            case 3:
+                printf("Node: ");
+                src = inputChar();
+                res = searchTree(tree, src);
+                res == NULL ? printf("Node not found!!\n") : printf("Node Found!!\n");
+                break;
+            case 4:
+                printf("Node: ");
+                src = inputChar();
+                res = searchParent(NULL, tree, src);
+                res == NULL ? printf("Node not found!!\n") : printf("Parent is Node %c\n", res->data);
+                break;
+            case 5:
+                showTree("Tree Status", tree);
+                break;
+            case 6:
+                looping = 0;
+                break;
+        }
+    }
 }
---
+++ DS/test.c
@@ -0,0 +1,11 @@
+// Author: Gokul Raj, 235, R3A, CSE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARR_LEN 256
+
+void main(){
+    int a = 0;
+    printf("%d %d", a, !a); 
+}
